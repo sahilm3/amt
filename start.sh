6b0f20087cd4e01da1e1867d24df51b4f02c9dff
sahilm3 <90495590+sahilm3@users.noreply.github.com>
2021-12-20 03:58:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -43,8 +43,4 @@ test -f $(pwd)/aria.conf-e && rm $(pwd)/aria.conf-e
 
 aria2c --conf-path=aria.conf
 echo "Aria2c daemon started"
-
-# Only start the bot if deployed to heroku, as in local the start command might be different for development
-if [[ -n $DYNO ]]; then
-	npm start
-fi
+npm start
